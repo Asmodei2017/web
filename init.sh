20bb1161725010e225706c8bfc1025a697ab6e9f
Asmodei2017 <p.zabavskiy@tut.by>
2018-02-14 15:08:37 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo fuser -k 80/tcp
+sudo /etc/init.d/nginx start
